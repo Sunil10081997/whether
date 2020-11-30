34bbba428e62432087ac68e3c6e17a850311dc25
Sunil10081997 <60805657+Sunil10081997@users.noreply.github.com>
2020-11-30 19:08:14 +0530

    'init'

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class Whether extends React.Component{
+  constructor(props){
+    super(props);
+    this.state={
+      whetherArr:[
+        'sunday',
+        'Monday',
+        'Tuesday',
+        "wednesday",
+        'Thursday',
+        'Friday',
+        'saturday'
+      ]
+      
+    }
+    this.handleSubmit=this.handleSubmit.bind(this);
+  }
+  handleSubmit(event){
+    event.preventDefault();
+    this.setState={
+      value:this.state.whetherArr
+    }
+  }
+
+  onClick=()=>{
+    const days=this.state.whetherArr.map(days=()=>{
+    if(days===sunday){
+      this.setState={
+        days:"sunday temprature=36c"
+      }
+    }
+    
+    else if(days===monday){
+      this.setState={
+        monday:"monday temperture 47C"
+      }
+    }
+    })
+  }
+
+  whetherForSunday(){
+    
+  }
+
+render(){
+  return(
+    <div>
+      <form onSubmit={this.handleSubmit}>
+        <input type='text' placeholder='Enter the Address name'
+        onClick={this.onClick}
+        />
+        <button type='submit'>Search</button>
+      </form>
     </div>
-  );
+  )
+}
 }
 
-export default App;
+export default Whether;
